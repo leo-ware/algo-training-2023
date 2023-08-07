e85a288c607c5abc29b1dfa1c484a3c142cc582b
Leo Ware <54528853+leo-ware@users.noreply.github.com>
2023-08-07 14:04:41 -0400

    Create 125. Valid Palindrome.py

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,25 @@
+# fp
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(filter(lambda c: c.isalnum(), s)).lower()
+        return s == "".join(reversed(s))
+
+# two pointers
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+            elif not s[j].isalnum():
+                j -= 1
+            else:
+                if s[i].lower() != s[j].lower():
+                    return False
+                else:
+                    i += 1
+                    j -= 1
+        
+        return True
